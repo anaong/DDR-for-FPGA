492d7661cca5a662df175c8ebd98ce2b6bf4cfc2
vboxuser <vboxuser@CSE-x25-VBox-VM.myguest.virtualbox.org>
2023-03-02 17:54:36 -0800

    added skeleton/initial files

---
+++ ddr/ice40/top_ddr.sv
@@ -0,0 +1,119 @@
+//DDR Project for UCSC CSE x25 taught by Dustin Richmond
+//By: Andre Ong
+//Used a lot of Will Green Pong project as a skeleton
+//Link: https://projectf.io/posts/fpga-pong/
+
+`default_nettype none
+`timescale 1ns / 1ps
+
+module top_ddr (
+    input  wire logic clk_12m_i,      // 12 MHz clock
+    input  wire logic btn_rst_i,      // reset button
+    input  wire logic btn_fire_i,     // fire button
+    input  wire logic btn_up_i,       // up button
+    input  wire logic btn_dn_i,       // down button
+    output      logic dvi_clk_o,      // DVI pixel clock
+    output      logic dvi_hsync_o,    // DVI horizontal sync
+    output      logic dvi_vsync_o,    // DVI vertical sync
+    output      logic dvi_de_o,       // DVI data enable
+    output      logic [3:0] dvi_r_o,  // 4-bit DVI red
+    output      logic [3:0] dvi_g_o,  // 4-bit DVI green
+    output      logic [3:0] dvi_b_o   // 4-bit DVI blue
+    );
+    //* verilator lint_off UNUSEDPARAM */
+    //* verilator lint_off UNUSEDSIGNAL */
+    //* verilator lint_off UNDRIVEN */
+
+    // gameplay parameters
+
+    // generate pixel clock
+    /* verilator lint_off PINCONNECTEMPTY */
+    logic clk_pix_l;
+    logic clk_pix_locked_l;
+    clock_480p clock_pix_inst (
+       .clk_12m(clk_12m_i),
+       .rst(btn_rst_i & btn_fire_i & btn_up_i & btn_dn_i),
+       .clk_pix(clk_pix_l),
+       .clk_pix_locked(clk_pix_locked_l)
+    );
+    /* verilator lint_off PINCONNECTEMPTY */
+
+    // display sync signals and coordinates
+    localparam CORDW = 10;  // screen coordinate width in bits
+    logic [CORDW-1:0] sx_l, sy_l;
+    logic hsync_l, vsync_l, de_l;
+    simple_480p display_inst (
+        .clk_pix(clk_pix_l),
+        .rst_pix(!clk_pix_locked_l),  // wait for clock lock
+        .sx(sx_l),
+        .sy(sy_l),
+        .hsync(hsync_l),
+        .vsync(vsync_l),
+        .de(de_l)
+    );
+
+    // screen dimensions (must match display_inst)
+    localparam H_RES = 640;  // horizontal screen resolution
+    localparam V_RES = 480;  // vertical screen resolution
+
+    logic frame_l;  // high for one clock tick at the start of vertical blanking
+    always_comb frame_l = (sy_l == V_RES && sx_l == 0);
+
+    // scores
+
+    // drawing signals
+
+    // debounce buttons
+    logic left_l, up_l, down_l, right_l;
+    /* verilator lint_off PINCONNECTEMPTY */
+    debounce deb_left (.clk(clk_pix_l), .in(btn_fire_i), .out(left_l), .ondn(), .onup());
+    debounce deb_up (.clk(clk_pix_l), .in(btn_up_i), .out(up_l), .ondn(), .onup());
+    debounce deb_down (.clk(clk_pix_l), .in(btn_dn_i), .out(down_l), .ondn(), .onup());
+    debounce deb_right (.clk(clk_pix_l), .in(btn_rst_i), .out(right_l), .ondn(), .onup());
+    /* verilator lint_on PINCONNECTEMPTY */
+
+    // game state
+    //enum {} state_r, state_n;
+
+    // update game state
+    //always_ff @(posedge clk_pix_l) state_r <= state_n;
+
+    // draw the score
+
+    // paint colour
+    logic [3:0] paint_r_l, paint_g_l, paint_b_l;
+
+    // display colour: paint colour but black in blanking interval
+    logic [3:0] display_r_l, display_g_l, display_b_l;
+    always_comb begin
+        display_r_l = (de_l) ? paint_r_l : 4'h0;
+        display_g_l = (de_l) ? paint_g_l : 4'h0;
+        display_b_l = (de_l) ? paint_b_l : 4'h0;
+    end
+
+    // DVI Pmod output
+    SB_IO #(
+        .PIN_TYPE(6'b010100)  // PIN_OUTPUT_REGISTERED
+    ) dvi_signal_io [14:0] (
+        .PACKAGE_PIN({dvi_hsync_o, dvi_vsync_o, dvi_de_o, dvi_r_o, dvi_g_o, dvi_b_o}),
+        .OUTPUT_CLK(clk_pix_l),
+        .D_OUT_0({hsync_l, vsync_l, de_l, display_r_l, display_g_l, display_b_l}),
+        /* verilator lint_off PINCONNECTEMPTY */
+        .D_OUT_1()
+        /* verilator lint_on PINCONNECTEMPTY */
+    );
+
+    // DVI Pmod clock output: 180° out of phase with other DVI signals
+    SB_IO #(
+        .PIN_TYPE(6'b010000)  // PIN_OUTPUT_DDR
+    ) dvi_clk_io (
+        .PACKAGE_PIN(dvi_clk_o),
+        .OUTPUT_CLK(clk_pix_l),
+        .D_OUT_0(1'b0),
+        .D_OUT_1(1'b1)
+    );
+
+    //* verilator lint_off UNUSEDPARAM */
+    //* verilator lint_off UNUSEDSIGNAL */
+    //* verilator lint_off UNDRIVEN */
+endmodule
---
+++ lib/clock/README.md
@@ -0,0 +1,30 @@
+# Clock - Verilog Library
+
+Clock-related Verilog designs from [Project F](https://projectf.io), including PLL and clock domain crossing. You can freely build on these [MIT licensed](../../LICENSE) designs. Get an overview of the whole lib from the [Verilog Library blog](https://projectf.io/verilog-lib/).
+
+## Verilog Modules
+
+* [xd.sv](xd.sv) - clock domain crossing (CDC) with pulse [[docs](https://projectf.io//posts/lib-clock-xd/)]
+* Lattice iCE40 (12 MHz board clock)
+  * [ice40/clock_480p.sv](ice40/clock_480p.sv) - PLL clock generation for VGA 640x480 (25.125 MHz)
+* Xilinx 7 Series (100 MHz board clock)
+  * [xc7/clock_480p.sv](xc7/clock_480p.sv) - PLL clock generation for VGA 640x480 (25.2 MHz)
+  * [xc7/clock_720p.sv](xc7/clock_720p.sv) - PLL clock generation for 1280x720 (74.25 & 371.25 MHz)
+  * [xc7/clock_1080p.sv](xc7/clock_1080p.sv) - PLL clock generation for 1920x1080 (148.5 MHz & 742.5 MHz)
+
+_The 5x clocks in the Xilinx 7 Series designs are used for DVI/HDMI TMDS encoding._
+
+Locate Vivado test benches in the [xc7](xc7) directory.  
+Find other modules in the [Library](../).
+
+## Blog Posts
+
+If you're new to FPGA development, check out the explanation of clocks in [Hello Arty Part 2](https://projectf.io/posts/hello-arty-2/).
+
+The [FPGA Graphics](https://projectf.io/posts/fpga-graphics/) series makes extensive use of these clock generation modules.
+
+See [Simple Clock Domain Crossing](https://projectf.io/posts/lib-clock-xd/) for documentation of the **xd** module.
+
+## SystemVerilog?
+
+These modules use a little SystemVerilog to make Verilog more pleasant, see the main [Library README](../README.md#systemverilog) for details.
