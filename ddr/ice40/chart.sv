5924559577635a142f348d443b7e30a6b0d0061c
vboxuser <vboxuser@CSE-x25-VBox-VM.myguest.virtualbox.org>
2023-03-16 14:47:29 -0700

    fixed error in comment

--- ddr/ice40/chart.sv
+++ ddr/ice40/chart.sv
@@ -1,7 +1,7 @@
 //Displays the chart from chart.hex into the game
 //Takes hex value
-//Left 8 bits is which arrows to launch
-//Right 8 bits is when arrows will launch
+//Left 4 bits is which arrows to launch
+//Right 4 bits is when arrows will launch
 
 `default_nettype none
 `timescale 1ns / 1ps
